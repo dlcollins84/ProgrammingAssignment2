caabea1729e4f364ad523c0931414a75b870738c
dlcollins84 <dlcollins84@gmail.com>
2015-01-16 15:52:11 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Matrix inversion is usually a costly computation.
+## Write a pair of functions that cache the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse.
+      
+      m <<- x            ##this cache's the matrix itself in the global environment
+      minv <<- solve(m)  ##this cache's the matrix inverse in the global environment
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and 
+## the matrix has not changed), then cacheSolve should retrieve the inverse 
+## from the cache
+      
+      
+      if(exists("m") && exists("minv") && !is.null(m) && !is.null(minv)){
+      ## Tests if the solution has already been cached, the matrix and its inverse
+      ## should both be defined in the global environment, and not be NULL
+            
+            if(identical(m,x)){
+            ## Tests if the original matrix has changed by comparing against the 
+            ## matrix in the cache.
+                  message("Retrieving the inverse matrix from the cache...")
+                  minv
+            } else{
+                  message("Original matrix has changed since last cached, 
+                          calculating the inverse matrix...")
+                  makeCacheMatrix(x)
+                  minv
+            }
+            
+      } else { 
+            message("Inverse matrix not found in cache, calculating the inverse matrix...")
+            makeCacheMatrix(x)
+            minv
+      }
 }
